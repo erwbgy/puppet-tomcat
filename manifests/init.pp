2872abe2db51f9e38801f723801838ae3fa90b09
Keith Burdis <keith@burdis.org>
2012-10-26 13:30:24 +0100

    Enable config lookup in hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,28 +3,62 @@ class tomcat (
   $user      = 'tomcat',
   $group     = 'tomcat',
   $basedir   = '/opt',
-  $workspace = '/root/tomcat'
+  $workspace = '/root/tomcat',
+  $use_hiera = false
 ) {
-  if $version == undef {
+  if $use_hiera {
+    $tomcat = hiera['tomcat']
+    if $tomcat == undef {
+      fail('tomcat hash not found in hiera config')
+    }
+    $_version = $tomcat['version'] ? {
+      undef   => $version,
+      default => $version
+    }
+    $_user = $tomcat['user'] ? {
+      undef   => $user,
+      default => $user
+    }
+    $_group = $tomcat['group'] ? {
+      undef   => $group,
+      default => $group
+    }
+    $_basedir = $tomcat['basedir'] ? {
+      undef   => $basedir,
+      default => $basedir
+    }
+    $_workspace = $tomcat['workspace'] ? {
+      undef   => $workspace,
+      default => $workspace
+    }
+  }
+  else {
+    $_version   = $version
+    $_user      = $user
+    $_group     = $group
+    $_basedir   = $basedir
+    $_workspace = $workspace
+  }
+  if $_version == undef {
     fail('tomcat version parameter is required')
   }
-  file { $workspace:
+  file { $_workspace:
     ensure  => directory,
   }
   exec { 'tomcat-basedir':
-    command => "/bin/mkdir -p ${basedir}",
-    creates => $basedir,
+    command => "/bin/mkdir -p ${_basedir}",
+    creates => $_basedir,
   }
-  Class['tomcat'] -> Class['sunjdk']
+  #Class['tomcat'] -> Class['sunjdk']
   class { 'tomcat::install':
-    version   => $version,
-    user      => $user,
-    group     => $group,
-    basedir   => $basedir,
-    workspace => $workspace,
+    version   => $_version,
+    user      => $_user,
+    group     => $_group,
+    basedir   => $_basedir,
+    workspace => $_workspace,
   }
   class { 'tomcat::config':
-    version   => $version,
-    basedir   => $basedir,
+    version   => $_version,
+    basedir   => $_basedir,
   }
 }
